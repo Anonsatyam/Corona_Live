a056f25bb700b7a9df97c07bccf57777e7efe3d8
Anonsatyam <satyamdeveloper98@gmail.com>
2020-03-24 22:40:33 +0530

    Service Worker Added

--- sw.js
+++ sw.js
@@ -12,49 +12,43 @@ const assets = [
     'images/fever.png',
 ];
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('./sw.js');
-    });
-  }
-
-// const staticCacheName = 'static-site-v2';
-// const dynamicCache = 'dynamic-site-v1';
+const staticCacheName = 'static-site-v2';
+const dynamicCache = 'dynamic-site-v1';
 
 
-// self.addEventListener('install', evt => {
-//     //console.log('service worker has been installed');
-//     evt.waitUntil(
-//         caches.open(staticCacheName)
-//         .then(cache => {
-//             console.log('caching shell assets');
-//             return cache.addAll(assets);
-//         })
-//     );
-// });
+self.addEventListener('install', evt => {
+    //console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => {
+            console.log('caching shell assets');
+            return cache.addAll(assets);
+        })
+    );
+});
 
-// self.addEventListener('activate', evt => {
-//     // console.log('service worker has been activated');
-//     evt.waitUntil(
-//         caches.keys().then(keys => {
-//             return Promise.all(keys
-//                 .filter(key => key !== staticCacheName)
-//                 .map(key => caches.delete(key))
-//             )
-//         })
-//     );
-// });
+self.addEventListener('activate', evt => {
+    // console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
+});
 
-// self.addEventListener('fetch', evt => {
-//     // console.log('fetch event', evt);
-//     evt.respondWith(
-//         caches.match(evt.request).then(cacheRes => {
-//             return cacheRes || fetch(evt.request).then(fetchRes => {
-//                 return caches.open(dynamicCache).then(cache => {
-//                     cache.put(evt.request.url, fetchRes.clone());
-//                     return fetchRes;
-//                 })
-//             });
-//         })
-//     );
-// });
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
