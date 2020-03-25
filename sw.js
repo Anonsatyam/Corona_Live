98b2277c9c7643d066a81d306cbf37a0f7837dd6
Anonsatyam <satyamdeveloper98@gmail.com>
2020-03-25 10:44:06 +0530

    Share button Added

--- sw.js
+++ sw.js
@@ -27,28 +27,28 @@ self.addEventListener('install', evt => {
     );
 });
 
-// self.addEventListener('activate', evt => {
-//     // console.log('service worker has been activated');
-//     evt.waitUntil(
-//         caches.keys().then(keys => {
-//             return Promise.all(keys
-//                 .filter(key => key !== staticCacheName)
-//                 .map(key => caches.delete(key))
-//             )
-//         })
-//     );
-// });
+self.addEventListener('activate', evt => {
+    // console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
+});
 
-// self.addEventListener('fetch', evt => {
-//     // console.log('fetch event', evt);
-//     evt.respondWith(
-//         caches.match(evt.request).then(cacheRes => {
-//             return cacheRes || fetch(evt.request).then(fetchRes => {
-//                 return caches.open(dynamicCache).then(cache => {
-//                     cache.put(evt.request.url, fetchRes.clone());
-//                     return fetchRes;
-//                 })
-//             });
-//         })
-//     );
-// });
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
