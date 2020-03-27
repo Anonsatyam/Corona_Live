b2c5f0130fdd7fea85e7a26045d1b2cddbd5832b
Anonsatyam <satyamdeveloper98@gmail.com>
2020-03-27 21:54:11 +0530

    Service Worker Added

--- js/database.js
+++ js/database.js
@@ -243,9 +243,3 @@ $(document).ready(function() {
   });
 });
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(reg => console.log("service worker registered"))
-    .catch(err => console.log("service worker not registered", err));
-}
